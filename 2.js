23d918c3e6b465679cdec5a86814551fb22f2fa3
Nick Gibb <gibbnick@gmail.com>
2018-04-28 11:21:22 -0400

    Done problem 2

---
+++ 2.js
@@ -0,0 +1,28 @@
+// Even Fibonacci numbers
+// https://projecteuler.net/problem=2
+const arr = [1, 2];
+const evenSum = [2];
+
+if (!Array.prototype.last) {
+  Array.prototype.last = function () {
+    const last = this[this.length - 1];
+    const secondLast = this[this.length - 2];
+    return last + secondLast;
+  };
+}
+
+while (true) {
+  const lastTwoSum = arr.last();
+  if (lastTwoSum > 4000000) {
+    break;
+  } else {
+    arr.push(lastTwoSum);
+    if (lastTwoSum % 2 === 0) {
+      evenSum.push(lastTwoSum);
+    }
+  }
+}
+
+const sum = evenSum.reduce((a, b) => a + b, 0);
+
+console.log(`The sum of the even numbers in the series less than 4,000,000 is ${  sum}`);
