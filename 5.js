7b07afe44c38a42a605f9e7fc6f994f1e7c742b9
Nick Gibb <gibbnick@gmail.com>
2018-04-28 14:07:08 -0400

    Done Q5

---
+++ 5.js
@@ -0,0 +1,17 @@
+// Smallest multiple
+// https://projecteuler.net/problem=5
+function isDivUpToTwenty(j) {
+  for (let i = 2; i <= 20; i += 1) {
+    if (j % i !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+for (let t = 20; ; t += 1) {
+  if (isDivUpToTwenty(t) === true) {
+    console.log(t);
+    return false;
+  }
+}
